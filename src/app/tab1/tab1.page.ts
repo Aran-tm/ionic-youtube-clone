b7e0601bb4f454d9841eca0918da5bdcf9dca507
Aran-tm <armandortmontes@gmail.com>
2024-04-02 14:25:06 -0500

    adding logotype

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import homeData from '../../assets/data/home.json';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  videos: any[] = [];
+	segments: any[] = [];
 
-  constructor() {}
+  constructor() {
+    this.segments = [
+			'All',
+			'New to you',
+			'Gaming',
+			'Sitcoms',
+			'Computer program',
+			'Documentary',
+			'Music'
+		].map((val) => ({
+			title: val,
+			selected: false
+    }));
+    console.log(this.segments);
+
+    setTimeout(() => {
+			// this.selectSegment(0);
+			this.videos = homeData;
+		}, 5000);
+  }
 
 }
